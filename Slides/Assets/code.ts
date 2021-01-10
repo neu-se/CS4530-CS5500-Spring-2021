4f40d97054a31b634ff96473f4a2059e1df1fcdc
Mitchell Wand <wand@ccs.neu.edu>
2021-01-09 19:30:55 -0500

    working on Lesson 1.2

---
+++ Slides/Assets/code.ts
@@ -0,0 +1,13 @@
+
+let c = new Circle(initRadius)
+let a = c.diameter()
+
+let a = c.calculateDiameter
+
+table1.addItem(student1,grade1)
+
+interface Shirt {
+    color : Color   // the color of the shirt
+}
+
+let myShirt = {color: 0xff000} // my shirt
